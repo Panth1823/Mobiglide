197aafcc7a4279be6af2d0a29c598969744800bf
Panth1823 <panth1823@gmail.com>
2024-11-09 22:22:07 +0530

    new commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <>
+            <div className="footer-cover">
+                <div className="footer-main">
+                    <div className='footer-socials-tabs'>
+                        <div className='footer-tab-left'>
+                            <div className='mobiglide-text'><p>Mobiglide.</p></div>
+                            <div className='socials-logos'>
+                                <div className='logo-cover'>
+                                    <div className='facebook-logo'><svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <g id="ri:facebook-fill">
+                                            <path id="Vector" d="M9.33329 9.00004H11L11.6666 6.33337H9.33329V5.00004C9.33329 4.31337 9.33329 3.66671 10.6666 3.66671H11.6666V1.42671C11.4493 1.39804 10.6286 1.33337 9.76196 1.33337C7.95196 1.33337 6.66663 2.43804 6.66663 4.46671V6.33337H4.66663V9.00004H6.66663V14.6667H9.33329V9.00004Z" fill="white" />
+                                        </g>
+                                    </svg>
+                                    </div>
+                                </div>
+                                <div className='logo-cover'>
+                                    <div className='facebook-logo'><svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <g id="ri:facebook-fill">
+                                            <path id="Vector" d="M9.33329 9.00004H11L11.6666 6.33337H9.33329V5.00004C9.33329 4.31337 9.33329 3.66671 10.6666 3.66671H11.6666V1.42671C11.4493 1.39804 10.6286 1.33337 9.76196 1.33337C7.95196 1.33337 6.66663 2.43804 6.66663 4.46671V6.33337H4.66663V9.00004H6.66663V14.6667H9.33329V9.00004Z" fill="white" />
+                                        </g>
+                                    </svg>
+                                    </div>
+                                </div>
+                                <div className='logo-cover'>
+                                    <div className='facebook-logo'><svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <g id="ri:facebook-fill">
+                                            <path id="Vector" d="M9.33329 9.00004H11L11.6666 6.33337H9.33329V5.00004C9.33329 4.31337 9.33329 3.66671 10.6666 3.66671H11.6666V1.42671C11.4493 1.39804 10.6286 1.33337 9.76196 1.33337C7.95196 1.33337 6.66663 2.43804 6.66663 4.46671V6.33337H4.66663V9.00004H6.66663V14.6667H9.33329V9.00004Z" fill="white" />
+                                        </g>
+                                    </svg>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='footer-tab-right'>
+                            <div className='about-us-footer-tab'>About Us</div>
+                            <div className='services-footer-tab'>Services</div>
+                            <div className='careers-footer-tab'>Careers</div>
+                            <div className='contact-footer-tab'>Contact</div>
+                        </div>
+                    </div>
+                    <div className='footer-rights'>
+                        <div className='horizontal-line'></div>
+                        <div className='rights-text-main'>
+                            <div>2024 All rights reserved</div>
+                            <div>Crafted by The Koios Studio</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Footer
